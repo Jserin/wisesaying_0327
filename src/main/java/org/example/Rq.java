ffac1bc7b5c6301ae362583763663bb7387c7f00
Jserin <jsr3643@naver.com>
2023-04-06 13:42:30 +0900

    re_level8_0406

--- src/main/java/org/example/Rq.java
+++ src/main/java/org/example/Rq.java
@@ -11,7 +11,7 @@ public class Rq {
 
     // 삭제?id=1&id=2 이런 식으로 여러 개 받을 수 있다고 가정
     // 밑은 삭제할 id값을 찾는 메소드
-    // 삭제뿐만 아니라 수정 목록 찾기 등에서도 사용함
+    // 삭제뿐만 아니라 수정 목록 찾기 등에서도 사용할거라 클래스 분리함
 
     public Rq(String command) {
         String[] commandBits = command.split("\\?", 2);
@@ -21,15 +21,17 @@ public class Rq {
         */
         actionCode = commandBits[0]; // [삭제, id=1&id=2] 중에서 [삭제]만 저장
 
-        params = new HashMap<>();
+        params = new HashMap<>(); //Rq를 호출하면 자동으로 params 해시맵 선언
 
-        // ?붙은 명령 아닐 때 반복문 돌기 전에 종료 시킴. ?랑 &가 없는데 밑 로직 들어가면 오류남
+        // ?로 두개로 나눠지지 않고 하나만 있을 때 반복문 돌기 전에 종료 시킴. ?랑 &가 없는데 밑 로직 들어가면 오류남
         if (commandBits.length == 1) return;
 
         String[] paramsBits = commandBits[1].split("&"); // [id=1, id=2]
 
+        // paramsBits 배열의 수만큼 반복하는 for문
         for (String paramStr : paramsBits) {
             String[] paramStrBits = paramStr.split("=", 2);
+            //paramStrBits = (1회전) [id, 1], (2회전) [id, 2]
 
             // 안정성을 위해 추가. 없어도 실행됨
             if (paramStrBits.length == 1) continue;
@@ -38,7 +40,7 @@ public class Rq {
             String value = paramStrBits[1];
             // id와 값으로 나누어서 params 해시맵에 저장
             params.put(key, value);
-        } // [id, 1]
+        } // {(id, 1), (id,2)}
     }
 
 
@@ -46,10 +48,12 @@ public class Rq {
         return actionCode;
     }
 
+    // id 값 받고 해시맵 params에서 value 반환하는 메소드
     public String getParam(String name) {
         return params.get(name);
     }
 
+    // getParam으로 value를 반환시켰는데 숫자가 아닐 때 메세지 출력하는 메소드. 오류발생 시 디폴드 다시 반환. -1
     public int getIntParam(String name, int defaultValue) {
         try {
             return Integer.parseInt(getParam(name));
--- src/main/java/org/example/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/org/example/wiseSaying/controller/WiseSayingController.java
@@ -13,6 +13,14 @@ public class WiseSayingController {
     private long lastWiseSayingId;
     private final List<WiseSaying> wiseSayings;
 
+
+    public WiseSayingController() {
+        // 클래스 선언 시 자동 실행 매개변수 필수
+        lastWiseSayingId = 0;
+        wiseSayings = new ArrayList<>();
+    }
+
+
     private WiseSaying findById(int id) {
         for (WiseSaying wiseSaying : wiseSayings) {
             if (wiseSaying.getId() == id) {
@@ -20,12 +28,6 @@ public class WiseSayingController {
             }
         }
         return null;
-    }
-
-    public WiseSayingController() {
-        // 클래스 선언 시 자동 실행 매개변수 필수
-        lastWiseSayingId = 0;
-        wiseSayings = new ArrayList<>();
     }
 
 
@@ -39,6 +41,7 @@ public class WiseSayingController {
 
         WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
         wiseSayings.add(wiseSaying);
+        //
 
         System.out.println(id + "번 명언이 등록되었습니다.");
         lastWiseSayingId = id;
@@ -51,17 +54,23 @@ public class WiseSayingController {
         for (int i = wiseSayings.size() - 1; i >= 0; i--) {
             WiseSaying wiseSaying = wiseSayings.get(i);
             // wiseSaying 은 객체, 변수
-
+            //list안에서 사용할 리스트에서 데이터를 가져오는 변수 선언
+            // i번째 리스트의 주소를 가져옴. 리스트의 주소가 i번째인 상태에서 아이디, 명언, 작가 출력하는 메소드 사용
             System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
         }
     }
 
     public void remove(Rq rq) {
+
+        // 입력한 id값이 숫자 아닐 때, 미리 메세지 출력하고 명령 다시 받음
         int id = rq.getIntParam("id", -1);
+
+        // id의 값이 숫자가 아닐 때 출력. 오류먼저 출력하고 받은 값이 디폴트 -1 이면 if문 수행 아니면 다음으로 넘어감
         if (id == -1) {
             System.out.println("정수를 입력하세요");
             return;
         }
+
         // id 값으로 저장된 데이터를 가져옴
         WiseSaying wiseSaying = findById(id);
 
@@ -92,11 +101,11 @@ public class WiseSayingController {
         }
 
         System.out.printf("기존 명언 : %s\n", wiseSaying.getContent());
-        System.out.printf("명언 수정) ");
+        System.out.println("명언 수정) ");
         String content = Container.getScanner().nextLine().trim();
 
         System.out.printf("기존 작가 : %s\n", wiseSaying.getAuthorName());
-        System.out.printf("작가 수정) ");
+        System.out.println("작가 수정) ");
         String authorName = Container.getScanner().nextLine().trim();
 
         wiseSaying.setContent(content);
